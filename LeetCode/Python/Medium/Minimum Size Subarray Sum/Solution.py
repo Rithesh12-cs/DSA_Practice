f6cefca72e1f38cecb4735cb1fa24ec81842977f
Rithesh Billi <ritheshbilli@gmail.com>
2026-07-08 22:34:12 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Minimum Size Subarray Sum/Solution.py
+++ LeetCode/Python/Medium/Minimum Size Subarray Sum/Solution.py
@@ -1,6 +1,16 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        
+        l= 0
+        s, n = 0, len(nums)
+        l, res = 0, float("inf")
+        for r in range(n):
+            s += nums[r]
+            while s >= target:
+                res = min(res, r - l + 1)
+                s -= nums[l]
+                l += 1
+        return res if res!=float('inf') else 0
+
             
 
         
