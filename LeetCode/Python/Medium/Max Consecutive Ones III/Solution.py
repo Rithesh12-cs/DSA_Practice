a3579a4d6cc890e626b63fcb8c7d59a28da7a5dd
Rithesh Billi <ritheshbilli@gmail.com>
2026-07-11 19:29:48 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Max Consecutive Ones III/Solution.py
+++ LeetCode/Python/Medium/Max Consecutive Ones III/Solution.py
@@ -1,16 +1,12 @@
-class Solution:
-    def longestOnes(self, nums: List[int], k: int) -> int:
-        freq={}
-        c,mc=0,0
-        l=0
-        for i in range(len(nums)):
-            freq[nums[i]]=freq.get(nums[i],0)+1
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        freq={}
+        c,mc=0,0
+        l=0
+        for i in range(len(nums)):
+            freq[nums[i]]=freq.get(nums[i],0)+1
 
-            while freq.get(0,0) > k:
-                freq[nums[l]]-=1
-                if freq[nums[l]]==0:
-                    del freq[nums[l]]
-                l+=1
-            mc=max(mc,i-l+1)
-        return mc
-        
+            while freq.get(0,0) > k:
+                freq[nums[l]]-=1
+                if freq[nums[l]]==0:
+                    del freq[nums[l]]
