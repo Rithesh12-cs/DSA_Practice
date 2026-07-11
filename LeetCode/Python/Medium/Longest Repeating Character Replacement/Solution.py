ae18208036e1493778f2601b5d1fcfd48f3f8a7d
Rithesh Billi <ritheshbilli@gmail.com>
2026-07-11 20:09:28 +0530

    Add Medium Python Solution from LeetCode

---
+++ LeetCode/Python/Medium/Longest Repeating Character Replacement/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freq={}
+        l,mc=0,0
+        ma=0
+        for r in range(len(s)):
+            freq[s[r]]=freq.get(s[r],0)+1
+            ma=max(ma,freq[s[r]])
+            while (r-l+1)-ma > k:
+                freq[s[l]]-=1
+                if freq[s[l]]==0:
+                    del freq[s[l]]
+                l+=1
+                ma=max(freq.values()) if freq else 0
+            mc=max(mc,r-l+1)
+        return mc
