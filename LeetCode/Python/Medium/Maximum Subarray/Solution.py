584f3db24791826628cc1f32bf3cd7d4d8af261d
Rithesh Billi <ritheshbilli@gmail.com>
2026-07-17 21:56:35 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Maximum Subarray/Solution.py
+++ LeetCode/Python/Medium/Maximum Subarray/Solution.py
@@ -1,4 +1,8 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        cur,
+        cur,masum=nums[0],nums[0]
+        for i in range(1,len(nums)):
+            cur = max(nums[i],cur+nums[i])
+            masum=max(masum,cur)
+        return masum
         
