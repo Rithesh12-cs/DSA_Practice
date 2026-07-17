8742193193a4131b951b7e0f8d827b856b071a1c
Rithesh Billi <ritheshbilli@gmail.com>
2026-07-17 21:58:45 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Maximum Subarray/Solution.py
+++ LeetCode/Python/Medium/Maximum Subarray/Solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        #Apply Kadane's Algorithm
         cur,masum=nums[0],nums[0]
         for i in range(1,len(nums)):
             cur = max(nums[i],cur+nums[i])
