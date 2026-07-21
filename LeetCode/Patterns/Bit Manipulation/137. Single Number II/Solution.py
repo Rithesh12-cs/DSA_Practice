387d9e207062dfac912ff72739c74b5ab1cf442a
Rithesh Billi <ritheshbilli@gmail.com>
2026-07-21 23:37:48 +0530

    Add Python Solution to Patterns/Bit Manipulation

---
+++ LeetCode/Patterns/Bit Manipulation/137. Single Number II/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq={}
+        res,n=0,len(nums)
+        for i in nums:
+            freq[i]=freq.get(i,0)+1
+        #for i in range(1,n+1):
+            cnt=freq.get(i,0)
+            if cnt ==1:
+                res=i
+        return res
+
+        
