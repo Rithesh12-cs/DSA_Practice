a25ea396772058635c8b2ecdab4704f735efeee7
Rithesh Billi <ritheshbilli@gmail.com>
2026-07-24 23:06:05 +0530

    Add Python Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.py
+++ LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x<=1:
-            return 0
+        if x<2:
+            return x
         l,r=1,x//2
         while l<=r:
             mid=(l+r)//2
